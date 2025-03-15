37fc791a0dc2b2c1e356601355be3c3f89855366
Edwin kipyego <koriredwin95@gmail.com>
2025-03-15 22:44:24 +0300

    Task-Lister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks'); 
+  
+ //submit
+ form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = taskInput.value;
+
+  if (taskDescription.trim() !== "") {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskDescription;
+    taskList.appendChild(taskItem);
+    taskInput.value = ""; // Clear the input field after adding the task
+  }
+});
 });
