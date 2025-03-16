643e0ccace7c5d845c6b9293948d0f1e816ed4a5
Edwin kipyego <koriredwin95@gmail.com>
2025-03-16 09:49:03 +0300

    corrected issues

--- src/index.js
+++ src/index.js
@@ -11,8 +11,37 @@ document.addEventListener("DOMContentLoaded", () => {
   if (taskDescription.trim() !== "") {
     const taskItem = document.createElement('li');
     taskItem.textContent = taskDescription;
-    taskList.appendChild(taskItem);
-    taskInput.value = ""; // Clear the input field after adding the task
+    //priority values
+    const priorityInput =document.getElementById("priority")
+    const priority = priorityInput.value;
+
+  // Apply color based on priority
+  if (priority === "high") {
+    taskItem.style.color = "red";
+} else if (priority === "medium") {
+    taskItem.style.color = "yellow";
+} else if (priority ==="low"){
+    taskItem.style.color = "green";
+} else {
+  console.log("unknown priority value:", priority);
+}
+
+
+// Create delete button
+const deleteButton = document.createElement('button');
+deleteButton.textContent = "❌";
+deleteButton.style.marginLeft = "10px";
+deleteButton.style.cursor = "pointer";
+deleteButton.addEventListener("click", () => {
+    taskList.removeChild(taskItem);
+});
+
+
+//Append delete button to task item
+ taskItem.appendChild(deleteButton);
+
+taskList.appendChild(taskItem);
+ taskInput.value = ""; // Clear the input field after adding the task
   }
 });
 });
